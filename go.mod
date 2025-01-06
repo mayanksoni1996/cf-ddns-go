ed22904956d1d9b14480a29985237a08330dc8de
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-01-05 21:38:17 -0700

    init(core): Commit to create files

---
+++ go.mod
@@ -0,0 +1,17 @@
+module cf-ddns
+
+go 1.23
+
+require (
+	github.com/cloudflare/cloudflare-go v0.113.0
+	github.com/getsentry/sentry-go v0.31.1
+)
+
+require (
+	github.com/goccy/go-json v0.10.4 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/time v0.8.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"github.com/cloudflare/cloudflare-go"
+	"github.com/getsentry/sentry-go"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func filterRecord(records []cloudflare.DNSRecord, recordName string) cloudflare.DNSRecord {
+	for _, record := range records {
+		if record.Name == recordName {
+			return record
+		}
+	}
+	return cloudflare.DNSRecord{}
+}
+func main() {
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: "https://fe302b95fa9f32abf654c6791ad774c7@o4508594522095616.ingest.de.sentry.io/4508594825658448",
+		// Set TracesSampleRate to 1.0 to capture 100%
+		// of transactions for tracing.
+		// We recommend adjusting this value in production,
+		TracesSampleRate: 1.0,
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+	}
+	resp, err := http.Get("https://api.ipify.org")
+	if err != nil {
+		log.Fatalf("Error getting IP: %s", err)
+	}
+	defer resp.Body.Close()
+	ip, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading IP: %s", err)
+	}
+	api, err := cloudflare.NewWithAPIToken(os.Getenv("CF_API_TOKEN"))
+	zoneName := os.Getenv("CF_ZONE_NAME")
+	zoneID, err := api.ZoneIDByName(zoneName)
+	if err != nil {
+		log.Fatalf("Error getting zone ID: %s", err)
+		sentry.CaptureException(err)
+	}
+	recordName := os.Getenv("CF_SUBDOMAIN") + "." + zoneName
+	//records, err := api.ListDNSRecords(context.Background(), zoneID, cloudflare.DNSRecord{Name: recordName})
+	records, _, err := api.ListDNSRecords(context.Background(), cloudflare.ZoneIdentifier(zoneID), cloudflare.ListDNSRecordsParams{Type: "A"})
+	if err != nil {
+		log.Fatalf("Error getting DNS records: %s", err)
+	}
+	record := filterRecord(records, recordName)
+	record.Content = string(ip)
+	resultSet, _ := api.UpdateDNSRecord(context.Background(), cloudflare.ZoneIdentifier(zoneID), cloudflare.UpdateDNSRecordParams{
+		Type:     record.Type,
+		Name:     recordName,
+		Content:  record.Content,
+		Data:     record.Data,
+		ID:       record.ID,
+		Priority: record.Priority,
+		TTL:      record.TTL,
+		Proxied:  record.Proxied,
+		Tags:     record.Tags,
+	})
+	log.Printf("Updated record: %s", resultSet.Name)
+}
